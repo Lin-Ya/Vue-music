f02061fd94ae5827c9ce956ce159dc8cbb803efa
Lin-Ya <fenghuiwing@outlook.com>
2018-05-13 23:02:45 +0800

    抓取轮播图数据

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -3,7 +3,7 @@ import jsonp from 'common/js/jsonp'
 import { commonParms, options} from './config'
 export function getRecommend() {
     const url = 'http://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-    const data = Object.assign({}, commonParams, {
+    const data = Object.assign({}, commonParms, {
         platform: 'h5',
         uin: 0,
         needNewCode: 1
