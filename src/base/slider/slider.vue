313edb482e5cf7838eb9625a6cf175fb0afa64e1
Lin-Ya <fenghuiwing@outlook.com>
2018-05-14 22:43:34 +0800

    轮播组件自动播放 改为使用跟教程一样的better-scroll版本

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,9 +4,11 @@
       <slot>
       </slot>
     </div>
-    <!-- <div class="dots">
-      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots" :key="item"></span>
-    </div> -->
+    <div class="dots">
+      <span class="dot" v-for="(item,index) in dots" :key="item"
+            :class="{active: currentPageIndex === index}"
+      ></span>
+    </div>
   </div>
 </template>
 
@@ -30,32 +32,84 @@ export default {
       default: 4000
     }
   },
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    };
+  },
   mounted() {
     setTimeout(() => {
       this._getSliderWidth();
+      this._initDots();
       this._initSlider();
+
+      if (this.autoPlay) {
+        this._play();
+      }
     }, 20);
+    window.addEventListener("resize", () => {
+      if (!this.slider) {
+        return;
+      }
+      this._setSliderWidth(true);
+      this.slider.refresh();
+    });
   },
   methods: {
     _getSliderWidth() {
-        this.children = this.$refs.sliderGroup.children
-        let width = 0
-        let sliderWidth = this.$refs.slider.clientWidth
+      this.children = this.$refs.sliderGroup.children;
+      let width = 0;
+      let sliderWidth = this.$refs.slider.clientWidth;
 
-        for(let i=0; i<this.children.length; i++){
-            let child = this.children[i]
-            addClass(child, 'slider-item')
-            child.style.width = sliderWidth + 'px'
-            width += sliderWidth
-        }
-        
-        if(this.loop){
-            width += 2* sliderWidth
-        }
-        this.$refs.sliderGroup.style.width = width + 'px'
-        
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i];
+        addClass(child, "slider-item");
+        child.style.width = sliderWidth + "px";
+        width += sliderWidth;
+      }
+
+      if (this.loop) {
+        width += 2 * sliderWidth;
+      }
+      this.$refs.sliderGroup.style.width = width + "px";
     },
-    _initSlider(){}
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: true,
+        snapLoop: this.loop,
+        snapThreshold: 0.3,
+        snapSpeed: 400
+      });
+      //提醒，这个slider是better-scroll的实例化对象，所以要查看官方文档里面的方法
+      this.slider.on("scrollEnd", () => {
+        let pageIndex = this.slider.getCurrentPage().pageX;
+        if (this.loop) {
+          pageIndex -= 1;
+        }
+        this.currentPageIndex = pageIndex;
+
+        if (this.autoPlay) {
+          clearTimeout(this.timer);
+          this._play();
+        }
+      });
+    },
+    _initDots() {
+      this.dots = new Array(this.children.length);
+    },
+    _play() {
+      let pageIndex = this.currentPageIndex + 1;
+      if (this.loop) {
+        pageIndex += 1;
+      }
+      this.timer = setTimeout(() => {
+        this.slider.goToPage(pageIndex, 0, 400);
+      }, this.interval);
+    }
   }
 };
 </script>
